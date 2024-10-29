5493e2c1f1ab353013716268b2fe783889d3fcf2
Junaid <mughaljunaidmughal136@gmail.com>
2024-10-29 22:55:41 +0500

    Some js Changes

--- BankTransations/wwwroot/js/site.js
+++ BankTransations/wwwroot/js/site.js
@@ -16,17 +16,17 @@
 
 
 //Show in Popup
-var showInPopup = (url, title) => {
-    $.ajax({
-        type: "GET",
-        url: url,
-        success: function (res) {
-            $("#form-modal .modal-body").html(res);
-            $("#form-modal .modal-title").html(title);
-            $("#form-modal").modal('show');
-        }
-    });
-}
+//var showInPopup = (url, title) => {
+//    $.ajax({
+//        type: "GET",
+//        url: url,
+//        success: function (res) {
+//            $("#form-modal .modal-body").html(res);
+//            $("#form-modal .modal-title").html(title);
+//            $("#form-modal").modal('show');
+//        }
+//    });
+//}
 
 
 //Add Or edit Transaction
